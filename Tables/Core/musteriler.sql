2c38ebbd40a37a035fd8ddaa136bfced9f19c96b
Umut Kahyaoğlu <umutk@Umut-MacBook-Air.local>
2026-05-19 04:51:25 +0300

    musteri tipi iliskisi eklendi

--- Tables/Core/musteriler.sql
+++ Tables/Core/musteriler.sql
@@ -1,10 +1,12 @@
 CREATE TABLE musteriler (
     id INT PRIMARY KEY IDENTITY(1,1),
+
     ad NVARCHAR(50) NOT NULL,
     soyad NVARCHAR(50) NOT NULL,
+
     tc_kimlik_no CHAR(11) UNIQUE,
     telefon CHAR(11) UNIQUE NOT NULL,
-    musteri_tipi_id INT NOT NULL,
+    
     olusturulma_tarihi DATETIME2,
     guncellenme_tarihi DATETIME2
 )
